85b6344250e86d2e1a2f55f1c538c6da32c86fa5
kamiyamashiki0 <ailelofficial@outlook.com>
2025-01-08 22:15:30 +0800

    singin

--- src/logIn.cpp
+++ src/logIn.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <conio.h>
+#include <sstream>
 
 logIn::logIn()
 {
@@ -21,6 +22,37 @@ void logIn::singIn()
     if (!ifs.is_open())
     {
         std::cout << "open file failed" << std::endl;
+    }
+    std::string s;
+    int flag = 0;
+    while(std::getline(ifs,s))//直接读整行，不要按逗号一块一块读了
+    {
+        std::stringstream ss;//设为局部变量，每次循环流都会被清空
+        std::string tmp_name;
+        ss << s;
+        std::getline(ss, tmp_name, ',');
+        if(name.compare(tmp_name)==0)
+        {
+            flag = 1;
+            std::cout << "请输入密码" << std::endl;
+            std::cin >> password;
+            std::string tmp_password;
+            std::getline(ss, tmp_password, ',');
+            if(password.compare(tmp_password)==0)
+            {
+
+            }
+
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if(flag==0)
+    {
+        std::cout << "账户不存在，请注册新账户" << std::endl;
     }
 }
 
