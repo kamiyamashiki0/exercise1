7513bec656fa6a26e0b7937c0bcbcdbe27f9a77f
kamiyamashiki0 <ailelofficial@outlook.com>
2025-01-23 13:23:38 +0800

    update

---
+++ build/CMakeFiles/exercise1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Git for Windows/github/exercise1/src/User.cpp" "CMakeFiles/exercise1.dir/User.cpp.obj" "gcc" "CMakeFiles/exercise1.dir/User.cpp.obj.d"
+  "D:/Git for Windows/github/exercise1/src/logIn.cpp" "CMakeFiles/exercise1.dir/logIn.cpp.obj" "gcc" "CMakeFiles/exercise1.dir/logIn.cpp.obj.d"
+  "D:/Git for Windows/github/exercise1/src/main.cpp" "CMakeFiles/exercise1.dir/main.cpp.obj" "gcc" "CMakeFiles/exercise1.dir/main.cpp.obj.d"
+  "D:/Git for Windows/github/exercise1/src/mainMenu.cpp" "CMakeFiles/exercise1.dir/mainMenu.cpp.obj" "gcc" "CMakeFiles/exercise1.dir/mainMenu.cpp.obj.d"
+  "D:/Git for Windows/github/exercise1/src/processControl.cpp" "CMakeFiles/exercise1.dir/processControl.cpp.obj" "gcc" "CMakeFiles/exercise1.dir/processControl.cpp.obj.d"
+  "D:/Git for Windows/github/exercise1/src/startMatch.cpp" "CMakeFiles/exercise1.dir/startMatch.cpp.obj" "gcc" "CMakeFiles/exercise1.dir/startMatch.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/exercise1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise1.dir/User.cpp.obj"
+  "CMakeFiles/exercise1.dir/User.cpp.obj.d"
+  "CMakeFiles/exercise1.dir/logIn.cpp.obj"
+  "CMakeFiles/exercise1.dir/logIn.cpp.obj.d"
+  "CMakeFiles/exercise1.dir/main.cpp.obj"
+  "CMakeFiles/exercise1.dir/main.cpp.obj.d"
+  "CMakeFiles/exercise1.dir/mainMenu.cpp.obj"
+  "CMakeFiles/exercise1.dir/mainMenu.cpp.obj.d"
+  "CMakeFiles/exercise1.dir/processControl.cpp.obj"
+  "CMakeFiles/exercise1.dir/processControl.cpp.obj.d"
+  "CMakeFiles/exercise1.dir/startMatch.cpp.obj"
+  "CMakeFiles/exercise1.dir/startMatch.cpp.obj.d"
+  "exercise1.exe"
+  "exercise1.exe.manifest"
+  "exercise1.pdb"
+  "libexercise1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
